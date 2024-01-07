c12dc4eca00be256109aeacd060691fcf19b3095
ismahaniy <116624956+ismahaniy@users.noreply.github.com>
2024-01-08 04:05:50 +0800

    Add files via upload

---
+++ L1Q2/src/l1q2/L1Q2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q2;
+
+/**
+ *
+ * @author User10
+ */
+public class L1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("NAME : NUR ISMAHANI BINTI MD.DAUD");
+        System.out.println("MATRIC NUMBER: U2102186");
+        System.out.println("E-MAIL: ismahanikhj@gmail.com");
+        System.out.println("ADRESS:  KAMPUNG PARIT RAJA SARI, \n\t BAKRI BATU 5, \n\t 84000 MUAR, JOHOR");
+        System.out.println("CONTACT: 018-7934523");
+        System.out.println("CALL ME: ISS");
+        // TODO code application logic here
+    }
+    
+}
